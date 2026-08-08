62df060be153e1dc5a473ec20258da54f7183c62
LucasCst1 <LucasCst1@github.com>
2026-08-08 15:55:44 -0400

    site5

--- api/index.php
+++ api/index.php
@@ -17,7 +17,7 @@ $str= "hello world";
 echo "<h1>Hellooo world!</h1></br>";
 echo "<h4>$str</h4>";
 ?>
-
+<a href="/wind.php">Em tailwind</a>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
