a93af763e9f9ae9997f523ab15f9d2fe713539fa
LucasCst1 <LucasCst1@github.com>
2026-08-08 20:02:04 -0400

    site7

--- api/index.php
+++ api/index.php
@@ -12,22 +12,16 @@
   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
 </svg>
+<nav>
+  <a href="wind.php">Home</a>
+  <a href="about.php">About</a>
+  <a href="contact.php">Contact</a>
+</nav>
 <?php
-$str= "hello world";
-echo "<h1>Hello world!</h1></br>";
-echo "<h4>$str</h4>";
-
-$request= $_SERVER['REQUEST_URI'];
-$basePath= $strtok($request, '?');
-
-switch($basePath) {
-  case '/':
-  case '/home':
-    require __DIR__ . '/wind.php';
-    break;
-}
+//$str= "hello world";
+//echo "<h1>Hello world!</h1></br>";
+//echo "<h4>$str</h4>";
 ?>
-<a href="./wind.php">Em tailwind</a>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
